aa46dd4d37e68b4c849c1ae4c6339ad1f6300783
SladkayaDoza <LeykoderTO@gmail.com>
2024-04-16 23:38:42 +0300

    Viewer

--- AFlipTheCat/view.ino
+++ AFlipTheCat/view.ino
@@ -1,11 +1,11 @@
 void viewer(String j) {
   bool displayUpdate = 1;
+  float cof = 100;
   JsonDocument rawSignal1;
   readJsonFromFile(objectDoc[j]["RawData"].as<const char*>(), rawSignal1); // Текст null
-  int size = sizeof(rawSignal1["RAW"].as<JsonArray>()) / sizeof(rawSignal1["RAW"].as<JsonArray>()[0]);
+  int size = rawSignal1["RAW"].as<JsonArray>().size();;
   
   while(1) {
-    int cof = 100;
     static uint8_t pointer = 0;
     tk();
     if (displayUpdate != 0) {
@@ -34,18 +34,19 @@ void viewer(String j) {
   }
 }
 
-void drawSignal(uint16_t cof, JsonArray arr, int size) {
+void drawSignal(float cof, JsonArray arr, int size) {
   int curx = 0;
   oled.setCursor(0, 0);
-  oled.print(11);
-  ooed.print(arr[1].as<int>());
+  oled.print((int)(curx + (float)(arr[1].as<int>())/cof));
+  oled.print(size);
   for (int i = 0; i < size; i++) {
+    if (curx > 128) return;
     if (arr[i] > 0) {
-      oled.fastLineH(16, curx, curx + arr[i].as<int>()*(1/cof));
-      curx += + arr[i].as<int>()*(1/cof);
+      oled.fastLineH(16, curx, (int)(curx + (float)(arr[i].as<int>())/cof));
+      curx += (float)((arr[i].as<int>())/cof);
     } else {
-      oled.fastLineH(48, curx, curx - arr[i].as<int>()*(1/cof));
-      curx -= arr[i].as<int>()*(1/cof);
+      oled.fastLineH(48, curx, (int)(curx - (float)(arr[i].as<int>())/cof));
+      curx -= (float)((arr[i].as<int>())/cof);
     }
   }
 }
