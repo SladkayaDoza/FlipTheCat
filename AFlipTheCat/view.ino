3ae32e5fb6bb55ef2591fed7b1b77c610d3ddf70
SladkayaDoza <LeykoderTO@gmail.com>
2024-04-17 00:27:17 +0300

    Viewer)

--- AFlipTheCat/view.ino
+++ AFlipTheCat/view.ino
@@ -1,8 +1,9 @@
-void viewer(String j) {
+void viewer(String j) {  // Ряжанка с яблоком, Злагода
   bool displayUpdate = 1;
   float cof = 100;
+  int flyx = 0;
   JsonDocument rawSignal1;
-  readJsonFromFile(objectDoc[j]["RawData"].as<const char*>(), rawSignal1); // Текст null
+  readJsonFromFile(objectDoc[j]["RawData"].as<const char*>(), rawSignal1);
   int size = rawSignal1["RAW"].as<JsonArray>().size();;
   
   while(1) {
@@ -12,41 +13,45 @@ void viewer(String j) {
       displayUpdate = 0;
       oled.clear();
       oled.home();
-      drawSignal(cof, rawSignal1["RAW"].as<JsonArray>(), size);
+      drawSignal(cof, rawSignal1["RAW"].as<JsonArray>(), size, flyx);
       oled.update();
     }
 
     if (up.click() or up.step()) {
-      cof+=10;
-      displayUpdate = 1;
-    }
-    if (down.click() or down.step()) {
       cof-=10;
       displayUpdate = 1;
     }
-    if (left.click() or left.step()) {
-      ;;
+    if (down.click() or down.step()) {
+      cof+=10;
+      displayUpdate = 1;
     }
-    if (right.click() or right.click()) {
-      ;;
+    if (left.click() or left.step()) {
+      flyx = constrain(flyx-cof/10, 0, 99999);
+      displayUpdate = 1;
+    }
+    if (right.click() or right.step()) {
+      flyx = constrain(flyx+cof/10, 0, 99999);
+      displayUpdate = 1;
     }
     if (back.click()) return;
   }
 }
 
-void drawSignal(float cof, JsonArray arr, int size) {
-  int curx = 0;
+void drawSignal(float cof, JsonArray arr, int size, int flyx) {
+  int curx = -flyx;
   oled.setCursor(0, 0);
   oled.print((int)(curx + (float)(arr[1].as<int>())/cof));
   oled.print(size);
   for (int i = 0; i < size; i++) {
     if (curx > 128) return;
     if (arr[i] > 0) {
-      oled.fastLineH(16, curx, (int)(curx + (float)(arr[i].as<int>())/cof));
+      oled.fastLineH(16, curx, curx + (int)(arr[i].as<int>())/cof);
       curx += (float)((arr[i].as<int>())/cof);
+      oled.fastLineV(curx, 16, 48);
     } else {
-      oled.fastLineH(48, curx, (int)(curx - (float)(arr[i].as<int>())/cof));
+      oled.fastLineH(48, curx, curx - (int)(arr[i].as<int>())/cof);
       curx -= (float)((arr[i].as<int>())/cof);
+      oled.fastLineV(curx, 16, 48);
     }
   }
 }
